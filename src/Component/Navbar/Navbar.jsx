6c61e755cbbe48fc3ff36463cb2119e54af4380f
yasinarafat4870 <yasinarafat4870@gmail.com>
2024-02-01 20:49:10 +0600

    Navbar part creating

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import Logo from "../../assets/Logo.png";
+import { IoSearchOutline } from "react-icons/io5";
+import Button from "../CommonComoponet/Button";
 
 const Navbar = () => {
   return (
     <>
-      <section className="navbar">
-        <div className="container"></div>
-      </section>
+      <nav className="navbar py-7">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            <div className="nav__logo">
+              <picture>
+                <img src={Logo} alt={Logo} />
+              </picture>
+            </div>
+            <div className="nav__menu flex  items-center gap-x-7">
+              <ul className="flex items-center gap-x-6">
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Our services</a>
+                </li>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">What’s new?</a>
+                </li>
+              </ul>
+
+              <div className="nav__contact flex gap-x-6 items-center">
+                <div>
+                  <IoSearchOutline />
+                </div>
+                <div>
+                  <Button>Contact us</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
